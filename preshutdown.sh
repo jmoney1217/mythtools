35ca2411572d52d0cf3f5012a6a31f5781361d27
Joe Swantek <joseph.swantek@gmail.com>
2015-05-26 11:28:33 -0500

    preshutdown: initial contribution
    Used in the preshutdown sequence of a mythtv backend to test if it
    is 'ok' to shutdown/suspend the machine. Checks if any user's are logged
    into the machine, if a transcode is running (i.e. mythbrake) or if
    mythicalLibrarian is running. If any of those items are active then
    shutdown is is halted.
    
    This can be simplified later by converting the shell script to python and
    interfacing with the mythtv backend and checking its Job queue.
    
    Signed-off-by: Joe Swantek <joseph.swantek@gmail.com>

---
+++ preshutdown.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# Check to see if anyone is currently logged in or if the machine was recently switched on.
+# Echoed text appears in log file. It can be removed and --quiet added to the
+# grep command once you are satisfied that mythTV is working properly.
+# Exit codes:-
+# 2 - Machine recently switched on, don't shut down.
+# 1 - A user is logged in, don't shut down.
+# 0 - No user logged in, OK to shut down.
+function checkLogin() {
+	# Customizable variables
+	MIN_UPTIME=10   # Minimum up time in minutes
+	# End of customizable variables
+
+	# Get a date/time stamp to add to log output
+	DATE=`date +%F\ %T\.%N`
+	DATE=${DATE:0:23}
+
+	UPTIME=`cat /proc/uptime | awk '{print int($1/60)}'`
+
+	if [ "$UPTIME" -lt "$MIN_UPTIME" ]; then
+		echo $DATE Machine uptime less than $MIN_UPTIME minutes, don\'t shut down.
+		return 2
+	fi
+
+	# Some configurations ( at least lxdm + xfce4) do not report GUI-logged-on users
+	# with "who" or "users".
+	# pgrep tests if processes named xfce* exist
+
+	XFCE_PROCS=`pgrep xfce`
+
+	USERS=`who -q | tail -n 1 | sed 's/[a-z #]*=//'`
+
+	if [ "$USERS" == "0" ] && [ "$XFCE_PROCS" == "" ]; then
+		echo $DATE No users are logged in, ok to shut down.
+		return 0
+	else
+		echo $DATE Someone is still logged in, don\'t shut down.
+		return 1
+
+	fi
+}
+
+# check to see if there are any transcode jobs running
+# 1 - A transcode job is running, don't shut down.
+# 0 - No transcode jobs running, OK to shut down.
+function checkTranscode() {
+	a=$(pidof -x mythbrake.sh)
+	if [ -n "$a" ]; then
+		echo $DATE mythbrake transcode running, don\'t shut down.
+		return 1
+	fi
+
+	return 0
+}
+
+# check to see if mythicalLibrarian is running
+# 1 - A librarian is running, don't shut down.
+# 0 - No librarian is running, OK to shut down.
+function checkLibrarian() {
+	a=$(pidof -x mythicalLibrarian)
+	if [ -n "$a" ]; then
+		echo $DATE mythicalLibrarian is running, don\'t shut down.
+		return 1
+	fi
+
+	return 0
+}
+
+checkTranscode
+ret=$?
+if [ $ret -ne 0 ]; then
+	echo $DATE "** preshutdown blocked, transcode in progress."
+	exit $ret
+fi
+checkLibrarian
+ret=$?
+if [ $ret -ne 0 ]; then
+	echo $DATE "** preshutdown blocked, librarian in progress."
+	exit $ret
+fi
+checkLogin
+ret=$?
+if [ $ret -ne 0 ]; then
+        echo $DATE "** preshutdown blocked, user still logged in."
+        exit $ret
+fi
+
+exit 0
+
