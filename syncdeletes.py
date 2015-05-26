af036857c9ed52e5e36b74c7f7bde28db0022ba1
Joe Swantek <joseph.swantek@gmail.com>
2015-05-26 01:12:48 -0500

    syncdeletes: delete orphaned mythtv recordings with no matching symlink
    The purpose of this script is to allow user's of mythicalLibrarian
    to delete the mythtv recordings that the mythicalLibrarian video
    symlinks link to. Currently, if a video symlink is deleted it will no
    longer show up in the user's "video library" but the original (and large)
    video and all it's related files still exist in the mythtv database.
    
    Finds all mythtv recordings that are no longer "attached" to a video
    symlink (most likely generated by mythicalLibrarian). Recordings that
    are still recording or have an active job are ignored. Any mythtv
    recording that is "orphaned" is deleted.
    
    Signed-off-by: Joe Swantek <joseph.swantek@gmail.com>

--- syncdeletes.py
+++ syncdeletes.py
@@ -1,97 +1,138 @@
 #!/usr/bin/env python2.7
 
-from MythTV import Job, Recorded, System, MythDB, MythBE, findfile, MythError, MythLog, datetime
-import sys, os, errno, time 
+from MythTV import Job, Recorded, System, MythDB, MythBE, MythError, MythLog
+import sys, os, errno
+import logging, logging.handlers
 from optparse import OptionParser
 
-myth_dir = '/home/mythtv/recordings'
-lib_dir = ['/home/mythtv/recordings/Episodes', '/home/mythtv/recordings/Movies', '/home/mythtv/recordings/Showings']
-exts = ['.mpg', '.mp4']
-notify_email = 'joseph.swantek@gmail.com'
+LIBDIR = ['/home/mythtv/recordings/Episodes', '/home/mythtv/recordings/Movies', '/home/mythtv/recordings/Showings']
+LOGFILE = '/home/mythtv/mythsyncdeletes.log'
+MAXLOGSIZE = 5 * 1024 * 1024
+MAXLOGS = 1
+
+class RecordingState:
+	Recorded, Recording, BusyJob, Autoexpire = range(4)
 
 class recording:
-	def __init__(self, mythfile):
-		self.file = mythfile
-		t = os.path.splitext(os.path.basename(mythfile))
-		self.chanid = t[0].split('_')[0]
-		# self.starttime = datetime.strptime(t[0].split('_')[1], "%Y%m%d%H%M%S")
-		self.starttime = t[0].split('_')[1]
-		self.ext = t[1].split('.')[1]
-		self.state = None
+	def __init__(self, mythrec):
+		self.mythrec = mythrec
+		self.metadata = mythrec.exportMetadata()
+		self.program = mythrec.getProgram()
 		self.lib_listing = None
+		self.state = RecordingState.Recorded
 
 	def dump(self):
-		print "%s: [%s, %s, %s][link: %s]" % (self.file, self.ext, self.chanid, self.starttime, self.lib_listing)
+		logging.debug("%s: [%s][link: %s]" % (self.metadata.filename, self.state, self.lib_listing))
+		logging.debug(self.metadata)
+		logging.debug(self.program)
+		if self.lib_listing:
+			self.lib_listing.dump()
 
-	def match(self, other):
-		if (self.chanid == other.chanid) and (self.starttime == other.starttime) and (self.ext == other.ext):
+	def match(self, filename):
+		if (os.path.split(self.metadata.filename)[1] == os.path.split(filename)[1]):
 			return True
 		else:
 			return False
 
-	def link(self, lib_listing):
-		self.lib_listing = lib_listing
-
-
 class lib_listing:
 	def __init__(self, listing):
 		self.listing = listing
-		self.recording = recording(os.readlink(listing))
+		self.symlink = os.readlink(listing)
 
 	def dump(self):
-		print "%s:" % (self.listing)
-		self.recording.dump()
+		logging.debug("%s: [%s]" % (self.listing, self.symlink))
 
 def main():
-	recordings = []
-	listings = []
-	' loop all files in myth_dir that are in ext and create recording '
-	for f in os.listdir(myth_dir):
-		if (os.path.splitext(f)[1] in exts): 
-			recordings.append(recording(f))
-	print "Recordings found: %s" % len(recordings)
-	for r in recordings:
-		r.dump()
-	
+	' setup logger, all to stdout and INFO and higher to LOGFILE '
+	logging.basicConfig(format='%(message)s',
+				level=logging.NOTSET)
+	loggingfile = logging.handlers.RotatingFileHandler(LOGFILE, maxBytes=(MAXLOGSIZE), backupCount=MAXLOGS)
+	loggingfile.setLevel(logging.INFO)
+	formatter = logging.Formatter('%(asctime)s: %(message)s', datefmt='%m-%d %H:%M')
+	loggingfile.setFormatter(formatter)
+	logging.getLogger('').addHandler(loggingfile)
+
+	' connect to mythtv database '
+	db = MythDB()
+	be = MythBE(db=db)
+
 	' loop all files in lib_dir that are symlinks and create listing '
-	for ld in lib_dir:	
+	listings = []
+	for ld in LIBDIR:
 		for dp, dn, files in os.walk(ld):
 			for file in files:
 				filepath = os.path.join(dp,file)
 				if (os.path.islink(filepath)):
 					listings.append(lib_listing(filepath))
-	print "Library Items found: %s" % len(listings)
-	# for l in listings:
-	#	l.dump()
 
-	' loop through the list of library items looking for matching recordings, linking them '
-	for l in listings:
-		for r in recordings:
-			if r.match(l.recording):
-				print "MATCH"
-				if r.lib_listing != None:
-					print "UH OH! Linking with something already linked!"
+	' get list of all recordings from MythDB, link with library, figure out their status '
+	recordings = []
+	activeRecordings = []
+	activeJobs = []
+	newDeleteList = []
+	mythrecordings = db.searchRecorded()
+	recorderList = be.getRecorderList()
+	for mrec in mythrecordings:
+		logging.debug(mrec)
+		rec = recording(mrec)
+		recordings.append(rec)
+
+		' loop through the list of library items looking for matching recordings, linking them '
+		for l in listings:
+			if rec.match(l.symlink):
+				if rec.lib_listing != None:
+					logging.error("UH OH! Linking with something already linked!")
 				else:
-					r.link(l)
-				r.dump()
-				l.recording.dump()
+					rec.lib_listing = l
 
-	' for all recordings that are not already deleted (autoexpire) and are now orphaned, set to delete (autoexpire) '
-	' be SURE that we dont screw up files that are being recorded now '
-	mydb = MythDB()
-	mybe = MythBE(db=mydb)	
-	#db = MythDB(args=(('DBHostName','localhost'),
-        #          ('DBName','mythconverg'),
-        #          ('DBUserName','mythtv'),
-        #          ('DBPassword','JL82QPNG')))
-	r = recordings[2]
-	print "[%s, %s]" % (r.chanid, r.starttime)
-	rec = Recorded((r.chanid, r.starttime), db=mydb)
-	print rec
+		' figure out if the recording is active '
+		for recorder in recorderList:
+			arec = be.getCurrentRecording(recorder)
+			if (arec['title'] is not None) and (arec['chanid'] is not 0) and \
+			   (arec == rec.program):
+				logging.debug(" - currently recording, skip")
+				activeRecordings.append(rec)
+				rec.state = RecordingState.Recording
+				break
+		if (rec.state != RecordingState.Recorded):
+			continue
 
-	' email new delete (autoxpire) list to me '
+		' figuire if the recording is part of an active job '
+		jobs = db.searchJobs() #need to generate jobs on each loop
+		for job in jobs:
+			if job.status in [Job.ABORTING, Job.ERRORING, Job.PAUSED, Job.PENDING, \
+					  Job.QUEUED, Job.RETRY, Job.RUNNING, Job.STARTING, Job.STOPPING]:
+				jobrec = Recorded((job.chanid, job.starttime), db=db)
+				if rec.mythrec == jobrec:
+					logging.debug(" - currently part of a job, skip")
+					activeJobs.append(rec)
+					rec.state = RecordingState.BusyJob
+					break
+		if (rec.state != RecordingState.Recorded):
+			continue
 
+		' potentially add to delete list, and delete orphaned recordings '
+		if (rec.lib_listing == None) and (rec.state == RecordingState.Recorded):
+			logging.debug(" - no link, delete")
+			newDeleteList.append(rec)
+			rec.mythrec.delete(force=True, rerecord=False)
 
+	' log summary '
+	logging.info("")
+	logging.info("** Summary **")
+	logging.info(" [MythDB Recordings][%s]" % len(recordings))
+	logging.info("  - active recordings: %s" % len(activeRecordings))
+	for arec in activeRecordings:
+		logging.info("   - %s" % arec.program)
+	logging.info("  - in progress jobs: %s" % len(activeJobs))
+	for ajobs in activeJobs:
+		logging.info("   - %s" % ajobs.program)
+
+	logging.info("")
+	logging.info(" [Mythical Links][%s]" % len(listings))
+	logging.info("  - new delete items: %s" % len(newDeleteList))
+	for d in newDeleteList:
+		logging.info( "   - %s" % d.program)
 
 if __name__ == '__main__':
 	main()
